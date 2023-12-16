2207dcca0594dd6c0b695fb938dbba3c350a274f
Ghulam-Muhammad-Mujtaba <gmmujtaba21@gmail.com>
2023-12-16 18:23:55 +0500

    Area CRUD

--- ETend/Areas/Admin/Controllers/AreaController.cs
+++ ETend/Areas/Admin/Controllers/AreaController.cs
@@ -39,5 +39,66 @@ namespace ETend.Areas.Admin.Controllers
 			}
 			return View(obj);
 		}
+		//GET
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var areaFromDbFirst = _unitOfWork.Area.GetFirstOrDefault(u => u.Id == id);
+			if (areaFromDbFirst == null)
+			{
+				return NotFound();
+			}
+			return View(areaFromDbFirst);
+		}
+
+		//POST
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public IActionResult Edit(Area obj)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.Area.Update(obj);
+				_unitOfWork.Save();
+				TempData["success"] = "Area updated successfully";
+				return RedirectToAction("Index");
+			}
+			return View(obj);
+		}
+
+		public IActionResult Delete(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			var areaFromDbFirst = _unitOfWork.Area.GetFirstOrDefault(u => u.Id == id);
+			if (areaFromDbFirst == null)
+			{
+				return NotFound();
+			}
+			return View(areaFromDbFirst);
+		}
+
+		//POST
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public IActionResult DeletePOST(int? id)
+		{
+			var obj = _unitOfWork.Area.GetFirstOrDefault(u => u.Id == id);
+			if (obj == null)
+			{
+				return NotFound();
+			}
+
+			_unitOfWork.Area.Remove(obj);
+			_unitOfWork.Save();
+			TempData["success"] = "Area deleted successfully";
+			return RedirectToAction("Index");
+
+		}
 	}
 }
