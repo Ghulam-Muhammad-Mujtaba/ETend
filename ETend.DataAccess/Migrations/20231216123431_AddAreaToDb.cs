8327a92ca1885fd151b725b5183de4bce7487f72
Ghulam-Muhammad-Mujtaba <gmmujtaba21@gmail.com>
2023-12-16 17:44:58 +0500

    Add project files.

---
+++ ETend.DataAccess/Migrations/20231216123431_AddAreaToDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ETend.DataAccess.Migrations
+{
+    public partial class AddAreaToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Areas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Areas", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Areas");
+        }
+    }
+}
---
+++ ETend.Models/Area.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETend.Models
+{
+	public class Area
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Name { get; set; }
+
+		[Required]
+		public AreaType Type { get; set; }
+
+		// Navigation Properties
+		//public ICollection<House> Houses { get; set; }
+		//public ICollection<Company> Companies { get; set; }
+		//public ICollection<RetailShop> RetailShops { get; set; }
+	}
+	public enum AreaType
+	{
+		Commercial,
+		Residential
+	}
+}
