6a7b158002df09e6de5009e652077adc1f3e49b0
Ghulam-Muhammad-Mujtaba <gmmujtaba21@gmail.com>
2023-12-20 23:16:06 +0500

    Product Home and Details (Before Identity)

--- ETend/Areas/Customer/Controllers/HomeController.cs
+++ ETend/Areas/Customer/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
-﻿using ETend.Models;
+﻿using ETend.DataAccess.Repository.IRepository;
+using ETend.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace ETend.Areas.Customer.Controllers
 {
@@ -8,17 +11,56 @@ namespace ETend.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll();
+            return View(products);
         }
+        public IActionResult Details(int productId)
+        {
+            ShoppingCart shoppingCart = new()
+            {
+                Count = 1,
+                //ProductId = productId,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == productId),
+            };
+            return View(shoppingCart);
+        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //[Authorize]
+        //public IActionResult Details(ShoppingCart shoppingCart)
+        //{
+        //    var claimsIdentity = (ClaimsIdentity)User.Identity;
+        //    var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+        //    shoppingCart.ApplicationUserId = claim.Value;
 
+        //    ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.GetFirstOrDefault(
+        //        u => u.ApplicationUserId == claim.Value && u.ProductId == shoppingCart.ProductId);
+
+        //    if (cartFromDb == null)
+        //    {
+        //        _unitOfWork.ShoppingCart.Add(shoppingCart);
+        //        _unitOfWork.Save();
+        //        HttpContext.Session.SetInt32(SD.SessionCart, _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value).ToList().Count);
+        //    }
+        //    else
+        //    {
+        //        _unitOfWork.ShoppingCart.IncrementCount(cartFromDb, shoppingCart.Count);
+        //        _unitOfWork.Save();
+        //    }
+        //    //_unitOfWork.Save();
+
+        //    return RedirectToAction(nameof(Index));
+        //}
         public IActionResult Privacy()
         {
             return View();
